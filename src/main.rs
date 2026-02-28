46448d42cf056c971319fa0f6967446448378510
erkang zhou <3226778019@qq.com>
2026-02-28 19:38:03 +0800

    init the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
