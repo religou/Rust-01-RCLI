1de047f89f7161fcdd1c3c47480454140504dd3a
erkang zhou <3226778019@qq.com>
2026-03-08 18:40:21 +0800

    feature: 更通用，不需要提前定义好数据结构，再进行 serialization

--- src/process.rs
+++ src/process.rs
@@ -1,28 +1,16 @@
 use csv::Reader;
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     println!("Processing CSV file: {}", input);
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let player: Player = result?;
-        ret.push(player);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value: Value = headers.iter().zip(record.iter()).collect();
+        ret.push(json_value);
     }
 
     let json = serde_json::to_string_pretty(&ret)?;
